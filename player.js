bbdf0e13a06f571e004b1b8905fdda226183a2ef
vvenkatesh2007 <72019079+vvenkatesh2007@users.noreply.github.com>
2021-01-30 12:03:05 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,49 @@
+class player {
+  constructor(){
+      this.index = null;
+      this.distance = 0;
+      this.name = null;
+      this.rank = null;
+  }
+
+  getCount(){
+      var playerCountRef = database.ref('playerCount');
+      playerCountRef.on("value",(data)=>{
+          playerCount = data.val();
+      })
+  }
+
+  updateCount(count){
+      database.ref('/').update({
+          playerCount: count
+      });
+  }
+
+  update(){
+      var playerIndex = "players/player" + this.index;
+      database.ref(playerIndex).set({
+          name:this.name,
+          distance:this.distance
+      });    
+    }
+
+  static getPlayerInfo(){
+      var playerInfoRef = database.ref('players');
+      playerInfoRef.on("value",(data)=>{
+          allPlayers = data.val();
+    })
+  }
+
+  getCraftAtEnd(){
+      database.ref('CraftAtEnd').on("value",(data)=>{
+          this.rank = data.val();
+      })
+  }
+
+ static updateCraftAtEnd(rank) {
+     database.ref('/').update({
+         CraftAtEnd:rank
+     })
+    }
+}
+ 
